2a159c621bb922a6455fd0c43077a566efe404bd
Rafael Santos <15151000+Blackmagicbox@users.noreply.github.com>
2025-08-06 20:15:32 +0200

    FEAT Update project structure and add Javalin server

--- src/main/java/com/moviewatchlist/app/App.java
+++ src/main/java/com/moviewatchlist/app/App.java
@@ -1,7 +1,12 @@
 package com.moviewatchlist.app;
 
+import io.javalin.Javalin;
+
 class App {
 	public static void main(String[] args) {
         System.out.println("Hello Javalings!");
+        Javalin app = Javalin.create()
+                .get("/", ctx -> ctx.result("Hello World"))
+                .start(8080);
     }
 }
