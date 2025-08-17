038c7b57d6c98dd4e6808a5d2429caeacb3c8317
Rafael Santos <15151000+Blackmagicbox@users.noreply.github.com>
2025-08-17 02:17:10 +0200

    FEAT Refactor HealthController to use a class structure and add getter methods; add Flyway dependency for database migrations

--- src/main/java/com/moviewatchlist/app/web/controllers/HealthController.java
+++ src/main/java/com/moviewatchlist/app/web/controllers/HealthController.java
@@ -4,7 +4,15 @@ import io.javalin.http.Context;
 
 import java.util.Map;
 
-public record HealthController(String version, String env) {
+public final class HealthController {
+    private final String version;
+    private final String env;
+
+    public HealthController(String version, String env) {
+        this.version = version;
+        this.env = env;
+    }
+
     public void health(Context ctx) {
         ctx.json(Map.of("status", "up", "Env", env, "version", version));
     }
