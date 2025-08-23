dc9089a986e728bb1c4d4cf8e4e09258504b33a4
Rafael Santos <15151000+Blackmagicbox@users.noreply.github.com>
2025-08-23 16:15:24 +0200

    Feature: Implement UserService for User business logic

---
+++ src/main/java/com/moviewatchlist/app/service/UserService.java
@@ -0,0 +1,23 @@
+package com.moviewatchlist.app.service;
+
+import com.moviewatchlist.app.domain.user.User;
+import com.moviewatchlist.app.repository.UserRepository;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class UserService {
+    public UserRepository userRepository;
+
+    public UserService() {
+        userRepository = new UserRepository();
+    }
+
+    public void saveUser(User user) throws SQLException {
+        userRepository.saveUser(user);
+    }
+
+    public List<User> findAllUsers() throws SQLException {
+        return userRepository.findAllUsers();
+    }
+}
